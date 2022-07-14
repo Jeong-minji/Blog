1e40caa1dd1ed1416a9a1108acaee1646232d855
Jeong-minji <alswl4152@naver.com>
2022-07-14 22:56:12 +0900

    Add: posting mdx to html

---
+++ pages/posts/[id].js
@@ -0,0 +1,35 @@
+import Layout from "../../components/Layout/Layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+const Post = ({ postData }) => {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
+
+export default Post;
